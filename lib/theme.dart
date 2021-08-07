92e20b21f8a0f33746879309e27a09950a983c4b
hamcuks <ivannurilham60@gmail.com>
2021-08-08 06:17:01 +0700

    create home page

---
+++ lib/theme.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class MyTheme {
+  static const Color scaffoldBackground = Color(0xFFFAFCFE);
+  static const Color green = Color(0xFF5ABF6E);
+  static const Color black = Color(0xFF0D0C11);
+
+  static const TextStyle appTitle = TextStyle(
+    fontSize: 32,
+    fontWeight: FontWeight.bold,
+    height: 1.6,
+    color: black,
+  );
+
+  static const TextStyle cardTitle = TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.bold,
+    color: Colors.white,
+  );
+
+  static const TextStyle regularText = TextStyle(
+      fontSize: 18, fontWeight: FontWeight.normal, color: Colors.white);
+}
